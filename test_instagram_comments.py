ce452f22cbedc55fe45eb9c98f1e903e999310bd
gsdo1004-cloud <gsdo1004@gmail.com>
2026-09-22 09:05:08 +0900

    feat(instagram): deploy contextual comment replies

--- test_instagram_comments.py
+++ test_instagram_comments.py
@@ -10,6 +10,14 @@ class InstagramCommentSafetyTests(unittest.TestCase):
         ok, _ = m.quality_ok(r, m.DEFAULT_CONFIG, [])
         self.assertTrue(ok)
         self.assertNotIn("http", r)
+    def test_media_context_shapes_reply(self):
+        r = m.safe_reply("저도 궁금해요", "user", "2027 재물운이 좋아지는 띠")
+        self.assertIn("재물운", r)
+    def test_homepage_domain_allowed(self):
+        c = {**m.DEFAULT_CONFIG, "blocked_terms": ["http://", "https://", "구매", "결제"]}
+        ok, _ = m.quality_ok("더 자세한 개인 운세가 궁금하시면 sajufortune.kr의 무료 운세도 참고해보세요 🔮", c, [])
+        self.assertTrue(ok)
+
     def test_near_duplicate(self):
         r = "댓글 남겨주셔서 감사합니다 😊 오늘도 편안한 하루 보내세요."
         ok, why = m.quality_ok(r, m.DEFAULT_CONFIG, [r])
